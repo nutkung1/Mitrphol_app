32b0efee0990d22a22832934d9208fd99c52569c
nutkung1 <78488006+nutkung1@users.noreply.github.com>
2023-09-21 01:07:06 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+from auto_gptq import AutoGPTQForCausalLM
 import torch
 
-model_name = "nutkung1/Mitr_Phol"
-tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=True)
-model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=True)
-generation_config = GenerationConfig.from_pretrained(model_name)
-
+if torch.cuda.is_available():
+  dev = "cuda:0"
+else:
+  dev = "cpu"
 # Streamlit app header
-st.title("Hugging Face Chatbot")
+st.title("Mitr Phol Chatbot")
 
 # Create a text input box for user messages
 user_input = st.text_input("You:", "")
@@ -16,13 +16,22 @@ user_input = st.text_input("You:", "")
 # Create a chat history area to display the conversation
 chat_history = st.empty()
 
+access_token = "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"
+model_name = "nutkung1/Mitr_Phol"
+offload_folder = "E:/app_mitrphol/Mitrphol_app-main/offload_folder"  # Specify the offload folder
+
+# Initialize the tokenizer and model with offload_folder
+tokenizer = AutoTokenizer.from_pretrained(model_name, token=access_token, offload_folder=offload_folder)
+model = AutoGPTQForCausalLM.from_quantized(model_name, token=access_token, offload_folder=offload_folder)
+generation_config = GenerationConfig.from_pretrained(model_name)
+
 def generate_chatbot_response(user_input, model, tokenizer):
     # Tokenize user input
-    input_ids = tokenizer.encode(user_input, return_tensors="pt")
+    input_ids = tokenizer.encode(user_input, return_tensors="pt").input_ids.to("cuda:0")
 
     # Generate a response from the model
     with torch.no_grad():
-        output = model.generate(input_ids)
+        output = model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=512)
 
     # Decode the model's response
     bot_response = tokenizer.decode(output[0], skip_special_tokens=True)
